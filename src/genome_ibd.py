7ba66a610b91557e71a05afc8e02eb3671d281b1
Desmond Teoh <maomaokong@gmail.com>
2018-10-08 15:20:13 +0800

    Minor Adjustment

--- src/genome_ibd.py
+++ src/genome_ibd.py
@@ -19,9 +19,6 @@ def compare_chromosomes(json_data, compare1, compare2):
 
     # Start comparing one character by character from first position
     for x, y in zip(chromosome1, chromosome2):
-        #print()
-        #print("x: {0}; y: {1}".format(x, y))
-
         if x == y:
             no_match += 1
 
@@ -36,30 +33,35 @@ def compare_chromosomes(json_data, compare1, compare2):
 
         matching_ind += 1
 
-        if (no_match) > 20:
+        if no_match > 20:
             print()
             print("No of matched: {0}".format(no_match))
             print("Matching Start: {0} and End: {1}".format(matching_start + 1, matching_end + 1))
 
 
-"""
-Read the configurations from setting file
-"""
-setting_file = os.path.dirname(os.getcwd()) + '/setting.json'
-with open(setting_file) as sf:
-    settings = json.load(sf)
+def main():
+    """
+    Read the configurations from setting file
+    """
+    setting_file = os.path.dirname(os.getcwd()) + '/setting.json'
+    with open(setting_file) as sf:
+        settings = json.load(sf)
 
-    data_folder_path = settings['data_folder_path']
-    data_file_name = settings['data_file_name']
-    accepted_polymorphisms = settings['accepted_polymorphisms']
-    first_chromosome = settings['first_chromosome']
-    second_chromosome = settings['second_chromosome']
+        data_folder_path = settings['data_folder_path']
+        data_file_name = settings['data_file_name']
+        accepted_polymorphisms = settings['accepted_polymorphisms']
+        first_chromosome = settings['first_chromosome']
+        second_chromosome = settings['second_chromosome']
 
-"""
-Read the sample data from data file
-"""
-data_file = os.path.dirname(os.getcwd()) + data_folder_path + data_file_name
-with open(data_file) as df:
-    chromosomes = json.load(df)
+    """
+    Read the sample data from data file
+    """
+    data_file = os.path.dirname(os.getcwd()) + data_folder_path + data_file_name
+    with open(data_file) as df:
+        chromosomes = json.load(df)
 
-compare_chromosomes(chromosomes, first_chromosome, second_chromosome)
+    compare_chromosomes(chromosomes, first_chromosome, second_chromosome)
+
+
+if __name__ == '__main__':
+    main()
